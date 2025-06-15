9664ee264b3c39fb1a9eb25fd65969f1e3c42cbd
AustinHoover <25714838+AustinHoover@users.noreply.github.com>
2025-06-15 19:37:20 -0400

    remove unneeded import

--- src/main/java/io/github/austinhoover/rpg/web/controller/MessageLogController.java
+++ src/main/java/io/github/austinhoover/rpg/web/controller/MessageLogController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.austinhoover.rpg.game.Global;
-import io.github.austinhoover.rpg.game.intent.GameLog.LogEntry;
 
 @RestController
 public class MessageLogController {
